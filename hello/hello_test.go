3ef348466e80e17b3ed22c832f593d01ebdbc9b5
nahuakang <kangnahua@gmail.com>
2020-07-02 22:43:51 +0200

    Learn Go with Tests - Hello, World

---
+++ hello/hello_test.go
@@ -0,0 +1,37 @@
+package main
+
+import "testing"
+
+// TestHello tests Hello function
+func TestHello(t *testing.T) {
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Nahua", "")
+		want := "Hello, Nahua"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Elodie", "French")
+		want := "Salut, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+}
