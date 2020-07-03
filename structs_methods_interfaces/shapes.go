7e57deb0ea06aa652545ac28e3a41ea764338fb4
nahuakang <kangnahua@gmail.com>
2020-07-04 00:09:09 +0200

    Refactor Perimeter and Area to use struct Rectangle

--- structs_methods_interfaces/shapes.go
+++ structs_methods_interfaces/shapes.go
@@ -1,11 +1,17 @@
 package main
 
+// Rectangle holds the height and width
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
 // Perimeter returns the perimeter given width and height
-func Perimeter(width float64, height float64) float64 {
-	return 2 * (width + height)
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
 }
 
 // Area returns the area given width and height
-func Area(width float64, height float64) float64 {
-	return width * height
+func Area(rectangle Rectangle) float64 {
+	return rectangle.Width * rectangle.Height
 }
