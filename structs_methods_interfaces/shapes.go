f72923b16072cb59dd1a0b2b98be7bdb1d73ab2b
nahuakang <kangnahua@gmail.com>
2020-07-05 00:05:38 +0200

    Use interface and refactor

--- structs_methods_interfaces/shapes.go
+++ structs_methods_interfaces/shapes.go
@@ -1,9 +1,20 @@
 package main
 
+import "math"
+
+// Shape is the generic interface
+type Shape interface {
+	Area() float64
+}
+
 // Rectangle holds the height and width
 type Rectangle struct {
 	Width  float64
 	Height float64
+}
+
+type Circle struct {
+	Radius float64
 }
 
 // Perimeter returns the perimeter given width and height
@@ -12,6 +23,11 @@ func Perimeter(rectangle Rectangle) float64 {
 }
 
 // Area returns the area given width and height
-func Area(rectangle Rectangle) float64 {
-	return rectangle.Width * rectangle.Height
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+// Area returns the area given radius
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
 }
