5919172c7044fbfdbc1efe7d35cae28fedc4be67
nahuakang <kangnahua@gmail.com>
2020-07-05 20:34:25 +0200

    Refactor unittests

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,6 +1,19 @@
 package main
 
+import "errors"
+
+// ErrNotFound stores an error for not found
+var ErrNotFound = errors.New("could not find the word you were looking for")
+
+// Dictionary is a custom type
+type Dictionary map[string]string
+
 // Search searches for a word in a map
-func Search(dictionary map[string]string, word string) string {
-	return dictionary[word]
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	if !ok {
+		return "", ErrNotFound
+	}
+
+	return definition, nil
 }
--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -3,16 +3,32 @@ package main
 import "testing"
 
 func TestSearch(t *testing.T) {
-	dictionary := map[string]string{"test": "this is just a test"}
-	got := Search(dictionary, "test")
-	want := "this is just a test"
+	dictionary := Dictionary{"test": "this is just a test"}
 
-	assertStrings(t, got, want)
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
+
+		assertStrings(t, got, want)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, got := dictionary.Search("unknown")
+
+		assertError(t, got, ErrNotFound)
+	})
+}
+
+func assertError(t *testing.T, got, want error) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got error %q want %q", got, want)
+	}
 }
 
 func assertStrings(t *testing.T, got, want string) {
-  t.Helper()
-  if got != want {
-    t.Errorf("got %q want %q given %q", got, want, "test")
-  }
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q given %q", got, want, "test")
+	}
 }
