cd25ff5e895e752501abe2516810661bbf1c6f27
nahuakang <kangnahua@gmail.com>
2020-08-14 22:26:55 +0200

    Use httptest to mock server

--- select/racer_test.go
+++ select/racer_test.go
@@ -1,10 +1,25 @@
 package racer
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
 
 func TestRacer(t *testing.T) {
-	slowURL := "http://www.facebook.com"
-	fastURL := "http://quii.co.uk"
+	slowerServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(20 * time.Millisecond)
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	// https://golang.org/pkg/net/http/httptest/#Server
+	slowURL := slowerServer.URL
+	fastURL := fastServer.URL
 
 	want := fastURL
 	got := Racer(slowURL, fastURL)
@@ -12,4 +27,7 @@ func TestRacer(t *testing.T) {
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
+
+	slowerServer.Close()
+	fastServer.Close()
 }
