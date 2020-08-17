e9265a479e5d3c23a260091324bb40352523ebf6
nahuakang <kangnahua@gmail.com>
2020-08-17 23:34:10 +0200

    Add test cases for non-string fields

---
+++ reflection/reflection_test.go
@@ -0,0 +1,51 @@
+package reflection
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestWalk(t *testing.T) {
+	cases := []struct {
+		Name          string
+		Input         interface{}
+		ExpectedCalls []string
+	}{
+		{
+			"Struct with one string field",
+			struct {
+				Name string
+			}{"Nahua"},
+			[]string{"Nahua"},
+		},
+		{
+			"Struct with two string fields",
+			struct {
+				Name string
+				City string
+			}{"Nahua", "Berlin"},
+			[]string{"Nahua", "Berlin"},
+		},
+		{
+			"Struct with non-string field",
+			struct {
+				Name string
+				Age  int
+			}{"Nahua", 29},
+			[]string{"Nahua"},
+		},
+	}
+
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
+			walk(test.Input, func(input string) {
+				got = append(got, input)
+			})
+
+			if !reflect.DeepEqual(got, test.ExpectedCalls) {
+				t.Errorf("got %v want %v", got, test.ExpectedCalls)
+			}
+		})
+	}
+}
