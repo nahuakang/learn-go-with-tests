49b3ec8ca29c63b225f18e4152cd7b6d457ce5c2
nahuakang <kangnahua@gmail.com>
2020-08-21 00:48:28 +0200

    Refactor and add test to nested fields

--- reflection/reflection_test.go
+++ reflection/reflection_test.go
@@ -5,6 +5,16 @@ import (
 	"testing"
 )
 
+type Person struct {
+	Name    string
+	Profile Profile
+}
+
+type Profile struct {
+	Age  int
+	City string
+}
+
 func TestWalk(t *testing.T) {
 	cases := []struct {
 		Name          string
@@ -33,6 +43,14 @@ func TestWalk(t *testing.T) {
 				Age  int
 			}{"Nahua", 29},
 			[]string{"Nahua"},
+		},
+		{
+			"Nested fields",
+			Person{
+				"Nahua",
+				Profile{29, "Berlin"},
+			},
+			[]string{"Nahua", "Berlin"},
 		},
 	}
 
