c05e07ef4ab8c6a8c3b6b9a5c6f2fb4db3a51802
nahuakang <kangnahua@gmail.com>
2020-08-23 00:50:14 +0200

    Implement concurrent Counter with WaitGroup and Mutex

--- sync/sync.go
+++ sync/sync.go
@@ -1,12 +1,22 @@
 package sync
 
+import "sync"
+
 // Counter type
 type Counter struct {
+	mu    sync.Mutex
 	value int
+}
+
+// NewCounter returns a new Counter
+func NewCounter() *Counter {
+	return &Counter{}
 }
 
 // Inc is a Counter method to increase value by 1
 func (c *Counter) Inc() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
 	c.value++
 }
 
